9f2099aeb87c320a023f1e6b4210a2a98299e8b4
EtanaAlemu <etanaalemunew@gmail.com>
2024-02-14 06:17:14 -0800

    fix

--- src/main/java/com/dxvalley/michudw/michudw/model/Transaction.java
+++ src/main/java/com/dxvalley/michudw/michudw/model/Transaction.java
@@ -6,12 +6,10 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 
-@Entity
+//@Entity
 @Data
 public class Transaction {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+
     private String bookingDate;
     private String txnAccount;
     private String reference;
