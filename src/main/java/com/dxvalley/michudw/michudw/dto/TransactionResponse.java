2c1a7e79985902dd0487f41016036aaa755fe1d9
EtanaAlemu <etanaalemunew@gmail.com>
2024-02-14 21:55:41 -0800

    fix

--- src/main/java/com/dxvalley/michudw/michudw/dto/TransactionResponse.java
+++ src/main/java/com/dxvalley/michudw/michudw/dto/TransactionResponse.java
@@ -1,17 +1,33 @@
 package com.dxvalley.michudw.michudw.dto;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
 public class TransactionResponse {
-    private String bookDate;
+//    private String bookDate;
+//    private String reference;
+//    private String descript;
+//    private String narrative;
+//    private String valueDate;
+//    private String debit;
+//    private String credit;
+//    private String from;
+//    private String to;
+//    private String closingBalance;
+
+    private String bookingDate;
+    private String txnAccount;
     private String reference;
-    private String descript;
-    private String narrative;
+    private String description;
+    private String narative;
     private String valueDate;
     private String debit;
     private String credit;
-    private String from;
-    private String to;
+    private String fromacct;
+    private String toacct;
+    private String bbf;
     private String closingBalance;
 }
